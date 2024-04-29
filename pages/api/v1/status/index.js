6f26ffbffabe082f3d40dd8517bf9d49ffd2937e
Cezar Molinar <cezarmolinar@gmail.com>
2024-04-29 09:49:31 -0300

    make `database.js` more robust to error handling

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,32 @@
 import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('select 1+1 as sum;')
-  console.log(result.rows)
+  const updateAt = new Date().toISOString()
 
-  response.status(200).json({ chave: 'Esta é a primeira API' })
+  const maxConnectionsResult = await database.query('show max_connections;')
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections)
+
+  const databaseVersionResult = await database.query('SHOW server_version;')
+  const version = databaseVersionResult.rows[0].server_version
+
+  const databaseName = process.env.POSTGRES_DB
+
+  const openedConnectionsResult = await database.query({
+    text: 'select count(*)::int from pg_stat_activity where datname = $1;',
+    values: [databaseName]
+  })
+  const openedConnections = openedConnectionsResult.rows[0].count
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        max_connections: maxConnections,
+        opened_connections: openedConnections,
+        version: version
+      }
+    }
+  })
 }
 
 export default status
